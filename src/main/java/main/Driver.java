bcb76e9d44f7bdf626d241251a836bb54380abee
DhirajBhakta <dhirajbhakta110@gmail.com>
2018-04-30 21:01:23 +0000

    final

--- src/main/java/main/Driver.java
+++ src/main/java/main/Driver.java
@@ -1,8 +1,7 @@
 package main;
 import preprocessing.preprocessor;
 
-
-
+import java.util.ArrayList;
 import java.util.List;
 
 import com.mashape.unirest.http.HttpResponse;
@@ -23,7 +22,7 @@ public class Driver {
 		System.out.println("SUMMARY:"+filename+"------------->"+output_dir.getName()+"::::"+stats);
 		StringBuilder summary = new StringBuilder("");
 		for(Sentence S:sentences)
-			summary.append(S.sentence);
+			summary.append(S.sentence+"\n");
 		try (PrintStream out = new PrintStream(new FileOutputStream(output))){ 
 			out.print(summary);
 		}
@@ -67,7 +66,9 @@ public class Driver {
 		return dir;		
 	}
 	
-
+    public static void reposition(List<Sentence> sentences) {
+    	
+    }
 	
 
 	
@@ -76,16 +77,23 @@ public class Driver {
 		
 		File dir = new File(input_dir);
 		File[] dir_listings = dir.listFiles();
+		System.out.println("DIR sice:"+dir_listings.length);
 		
 		File fuzzy_output_dir = createDirectory("FUZZY");
 		File bushy_output_dir = createDirectory("BUSHY");
-		File wnet_output_dir = createDirectory("WORDNET");
+		File wnet_output_dir = createDirectory("WORDNET");		
+		File summary_output_dir = createDirectory("COMBO");
 		
-		
-		
+		int i=0;
 		for(File input_file: dir_listings) {
+				System.out.println("---------"+i);
+				i = i+1;
 		    	List<Sentence> sentences = new preprocessor(input_file).read_doc();
-		    	List<Sentence> summary;
+		    	List<Sentence> wnet_summary;
+		    	List<Sentence> fuzzy_summary;
+		    	List<Sentence> bushy_summary;
+		    	List<Sentence> summary = new ArrayList<Sentence>();
+		    	
 		    	
 		    	String stats;
 		    	Sentence title = sentences.get(0);
@@ -93,20 +101,43 @@ public class Driver {
 		    	
 		    	
 		    	WordNet wnet = new WordNet(sentences);
-		    	summary = wnet.getSummary();
+		    	wnet_summary = wnet.getSummary();
 		    	stats = wnet.getStats();
-		    	printSummary(input_file.getName(), summary, wnet_output_dir, stats);
-//		    		
-//		    	Fuzzy fuzzy = new Fuzzy(title, sentences);
-//		    	summary= fuzzy.getSummary();
-//		    	stats = fuzzy.getStats();
-//		    	printSummary(input_file.getName(), summary, fuzzy_output_dir, stats);
-//		  	
-//		    	BushyPath bushy = new BushyPath(sentences);
-//		    	summary = bushy.getSummary();
-//		    	stats = bushy.getStats();
-//		    	printSummary(input_file.getName(), summary, bushy_output_dir,stats);
-		}
+		    	printSummary(input_file.getName(), wnet_summary, wnet_output_dir, stats);
+		    		
+		    	Fuzzy fuzzy = new Fuzzy(title, sentences);
+		    	fuzzy_summary= fuzzy.getSummary();
+		    	stats = fuzzy.getStats();
+		    	printSummary(input_file.getName(), fuzzy_summary, fuzzy_output_dir, stats);
+		  	
+		    	BushyPath bushy = new BushyPath(sentences);
+		    	bushy_summary = bushy.getSummary();
+		    	stats = bushy.getStats();
+		    	printSummary(input_file.getName(), bushy_summary, bushy_output_dir,stats);
+		
+		    	int word_count = 0;
+		    	for(Sentence S: wnet_summary)
+		    		if(fuzzy_summary.contains(S) && bushy_summary.contains(S)) {
+		    			summary.add(S);
+		    			word_count += S.getWords().size();
+		    		}
+		    	
+		    	for(Sentence S: sentences) {
+		    		if(word_count>100)
+		    			break;
+		    		if(!summary.contains(S) &&(wnet_summary.contains(S) || fuzzy_summary.contains(S) || bushy_summary.contains(S))) {
+		    			summary.add(S);
+		    			word_count += S.getWords().size();
+		    		}
+		    	}
+		    	List<Sentence> _summary = new ArrayList<Sentence>();
+		    	for(Sentence S: sentences) 
+		    		if(summary.contains(S))
+		    			_summary.add(S);
+		    	summary = _summary;
+		    	printSummary(input_file.getName(), summary, summary_output_dir,"");
+		    	
+		    }
 		}
 
 }
